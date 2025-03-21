c499b701bb57171bd74db5b01c5514a6e621323e
Federico Falaguerra <ff@MacBook-Pro-di-Federico.local>
2025-03-21 16:54:31 +0100

    fixed

--- src/data/main.js
+++ src/data/main.js
@@ -14,17 +14,27 @@ if (isBrowser) {
     currentSlideIndex: 0, // Memorizza l'indice corrente
     carouselSwiper: null,
     closeLightBoxButton: null,
+    hero:  null,
+    headerContainer: null,
+    headerSticky: null,
+    bandaPreventivo: null,
 
     init: function () {
       // HEADER
       this.header = document.querySelector('header');
-      this.menuTrigger = document.querySelector('.menu-trigger');
+      this.menuTrigger = document.querySelectorAll('.menu-trigger');
       this.menuMobile = document.querySelector('.menu-mobile');
       this.menuItems = document.querySelectorAll('.menu-items li a');
       this.navigation = document.querySelector('.menu-items');
-      this.logo = document.querySelector('.logo');
+      this.logo = document.querySelectorAll('.logo');
       this.body = document.querySelector('body');
       this.closeLightBoxButton = document.querySelector('#close-lightbox');
+      this.headerContainer = document.querySelector('.header-sticky .container');
+      this.hero = document.querySelector('.hero')?.offsetHeight || 0;
+      this.heroSection = document.querySelector('.hero');
+      this.headerSticky = document.querySelector('.header-sticky');
+      this.bandaPreventivo = document.querySelector('.banda-preventivo');
+    
       
       // LIGHTBOX
       this.lightbox = document.getElementById("lightbox");
@@ -36,7 +46,38 @@ if (isBrowser) {
 
       this.addEventListeners();
       this.initFaq();
+      this.headerScroll();
+      this.handleStickyElement();
     },
+
+    headerScroll: function () {
+      const headerSticky = this.headerSticky;
+      const hero = this.hero;
+      let lastScrollTop = window.pageYOffset;
+    
+      window.addEventListener("scroll", () => {
+        let currentScroll = window.pageYOffset;
+    
+        if (currentScroll > hero) {
+          // Solo se abbiamo superato hero
+          if (currentScroll > lastScrollTop) {
+            // Scrollando in giù -> Nasconde l'header
+            headerSticky.classList.add('translate-y-[-300%]', 'opacity-0');
+          } else {
+            // Scrollando in su -> Mostra l'header
+            headerSticky.classList.remove('translate-y-[-300%]', 'opacity-0');
+          }
+        } else {
+          // Se siamo sopra hero, assicuriamoci che l'header sia sempre nascosto
+          headerSticky.classList.add('translate-y-[-300%]', 'opacity-0');
+        }
+    
+        lastScrollTop = currentScroll;
+      });
+    },
+    
+    
+
 
     initFaq: function () {
       const faqToggle = document.querySelectorAll('.faq-item');
@@ -60,6 +101,7 @@ if (isBrowser) {
         });
       });
     },
+
 
 
     initSwiperCarousel: function () {
@@ -107,7 +149,11 @@ if (isBrowser) {
 
     addEventListeners: function () {
       if (this.menuTrigger) {
-        this.menuTrigger.addEventListener('click', this.toggleMenu.bind(this));
+       // this.menuTrigger.addEventListener('click', this.toggleMenu.bind(this));
+        this.menuTrigger.forEach(trigger => {
+          trigger.addEventListener('click', this.toggleMenu.bind(this));
+        }
+        );
       }
 
       if (this.menuItems.length > 0) {
@@ -139,16 +185,24 @@ if (isBrowser) {
 
     toggleMenu: function () {
       const headerHeight = this.header?.getBoundingClientRect();
+      const headerContainer = this.headerContainer;
       this.body.classList.add('overflow-y-hidden');
 
       if (this.menuMobile?.classList.contains('opacity-0')) {
         this.navigation.style.paddingTop = `${headerHeight.height}px`;
-
         setTimeout(() => {
-          this.logo.src = "/src/assets/logo-white.svg";
+          this.headerContainer.classList.remove('bg-white', 'p-5');
+          this.logo.forEach(logo => {
+            logo.src = "/src/assets/logo-white.svg";
+          });
+          //this.logo.src = "/src/assets/logo-white.svg";
           this.menuMobile.classList.remove('opacity-0', 'invisible');
-          this.menuMobile.classList.add('opacity-100', 'visible', 'z-1');
-          this.menuTrigger.classList.add('active');
+          this.menuMobile.classList.add('opacity-100', 'visible', 'z-10');
+          //this.menuTrigger.classList.add('active');
+          this.menuTrigger.forEach(trigger => {
+            trigger.classList.add('active');
+          }
+          );    
         }, 300);
       } else {
         this.closeMenu();
@@ -157,11 +211,21 @@ if (isBrowser) {
 
     closeMenu: function () {
       this.body.classList.remove('overflow-y-hidden');
+      const headerContainer = this.headerContainer;
+
       setTimeout(() => {
-        this.logo.src = "/src/assets/logo.svg";
+        //this.logo.src = "/src/assets/logo.svg";
+        this.headerContainer.classList.add('bg-white', 'p-5');
+        this.logo.forEach(logo => {
+          logo.src = "/src/assets/logo.svg";
+        });
         this.menuMobile.classList.add('opacity-0');
-        this.menuMobile.classList.remove('opacity-100', 'visible', 'z-1');
-        this.menuTrigger.classList.remove('active');
+        this.menuMobile.classList.remove('opacity-100', 'visible', 'z-10');
+        //this.menuTrigger.classList.remove('active');
+        this.menuTrigger.forEach(trigger => {
+          trigger.classList.remove('active');
+        }
+        );   
       }, 300);
     },
 
@@ -184,7 +248,27 @@ if (isBrowser) {
 
       // Chiudi la lightbox senza resettare Swiper
       this.lightbox.classList.add("hidden");
-    }
+    },
+
+    handleStickyElement: function() {
+      const bandaPreventivo = this.bandaPreventivo;
+      const elementOriginalOffset = bandaPreventivo.offsetTop;
+    
+      window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY;
+    
+        if (scrollY + window.innerHeight < elementOriginalOffset) {
+          // Se il footer è visibile, posiziona l'elemento in basso
+          bandaPreventivo.classList.add('fixed', 'bottom-0', 'left-0', 'w-full');
+        } else {
+          // Se il footer non è visibile, ripristina la posizione originale
+          bandaPreventivo.classList.remove('fixed', 'bottom-0', 'left-0', 'w-full');
+        }
+         
+      });
+
+      console.log(elementOriginalOffset);
+    },
   };
 
   document.addEventListener('DOMContentLoaded', function () {
